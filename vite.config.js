efbf30c177938586df0316249383c20a62fbe38f
Panawat Srimuang <panawatzaa2544@gmail.com>
2026-09-14 14:16:03 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from '@vitejs/plugin-react'
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/project-shop/' : '/',
   plugins: [react()],
-  base: '/project-shop/'
 })
