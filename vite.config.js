3c2c9782a8ff8b94945e8be8500f27bd97d2ac24
Panawat Srimuang <panawatzaa2544@gmail.com>
2026-09-14 14:18:58 +0700

    back

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/project-shop/' : '/',
   plugins: [react()],
+  base: '/project-shop/'
 })
