af7b9cf43e2d943a459a11811beaae56a07a77a8
Panawat Srimuang <panawatzaa2544@gmail.com>
2026-09-14 11:53:14 +0700

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
-import Navbar from "./components/layout/Navbar";
 import Layout from "./components/layout/Layout";
 import Product from "./pages/Products/Products";
-// import Products from "./pages/Products/Products";
-// import Cart from "./pages/Cart/Cart";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/project-shop">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
-          {/* <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} /> */}
+
+          {/* path ที่ไม่มี ให้กลับ Home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Layout>
     </BrowserRouter>
